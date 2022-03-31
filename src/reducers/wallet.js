dc4cadbcba958f13248d68c0ec5deae39e7b7c66
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-03-31 14:57:34 -0300

    feat reducer wallet.js

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,14 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INIT_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+const wallet = (state = INIT_STATE, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default wallet;
