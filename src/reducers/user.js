1d4fd94efa1a04aa11c2ac9e87bea2637a461768
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-03-31 14:55:49 -0300

    feat reducers inuser.js

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,21 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { ADD_USER } from '../actions';
+
+const INIT_STATE = {
+  email: '',
+
+};
+
+const user = (state = INIT_STATE, action) => {
+  const { payload } = action;
+  switch (action.type) {
+  case ADD_USER:
+    return {
+      email: payload,
+    };
+  default:
+    return state;
+  }
+};
+
+export default user;
