96be2fb1a208cb130e6eb9929a1843b13fe37a45
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-03-31 14:54:24 -0300

    feat Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { userAction } from '../actions';
 
-class Login extends React.Component {
-  render() {
-    return <div>Login</div>;
-  }
+function validate(email, password, func) {
+  const MIN_VALUE = 6;
+  const validateEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  const validatePassword = password.length >= MIN_VALUE;
+  const inputs = [validateEmail, validatePassword];
+  const isValidade = inputs.every((input) => input === true);
+  if (isValidade) func(false);
+  // ref.: https://github.com/tryber/sd-019-c-live-lectures/blob/lecture/11.5/aula_extra/src/App.js
 }
 
-export default Login;
+function Login(props) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isDisabled, setIsDisabled] = useState(true);
+  const history = useHistory();
+
+  useEffect(() => { validate(email, password, setIsDisabled); }, [email, password]);
+
+  function handleClick() {
+    const { dispatch } = props;
+    dispatch(userAction(email));
+    history.push('/carteira');
+  }
+
+  return (
+    <fieldset>
+      <label htmlFor="email">
+        Email:
+        <input
+          type="email"
+          id="email"
+          data-testid="email-input"
+          name="email"
+          value={ email }
+          onChange={ (e) => setEmail(e.target.value) }
+        />
+      </label>
+      <label htmlFor="password">
+        Senha:
+        <input
+          type="password"
+          id="password"
+          data-testid="password-input"
+          name="password"
+          value={ password }
+          onChange={ (e) => setPassword(e.target.value) }
+        />
+      </label>
+      <button
+        type="button"
+        disabled={ isDisabled }
+        onClick={ () => handleClick() }
+      >
+        Entrar
+      </button>
+    </fieldset>
+  );
+}
+
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect()(Login);
